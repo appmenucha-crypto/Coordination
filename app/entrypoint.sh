8f26c0ed0f6b9f468ad43ec31ffab88b3c106494
appmenucha-crypto <appmenucha@gmail.com>
2026-03-16 12:49:12 +0000

    Update comment for Gunicorn server launch

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -7,5 +7,5 @@ python manage.py migrate
 # Collecter les fichiers statiques
 python manage.py collectstatic --noinput
 
-# Lancer le serveur Gunicorn sur le port fourni par Dokploy
+# Lancer Gunicorn sur le port fourni par Dokploy
 exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
